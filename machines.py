cdaad8dba0c80b360312ff6322ee833e2b1575fe
Kristiono Setyadi <ksetyadi@tracehouse-go.local>
2024-01-20 13:24:44 +0700

    Add Streamwave and remove unused server

--- machines.py
+++ machines.py
@@ -2,12 +2,8 @@ hostnames = [
 	['---------- PERSONAL ----------', '-----', '--', '----', '---------'],
 	['meowmagz', '206.189.208.77', '22', 'root', 'Usage: Meowmagz Server'],
 	['orion-sgp1', '167.99.67.233', '22', 'root', 'Usage: Orion Server'],
-	['ubuntu-api-gateway', '188.166.248.126', '22', 'root', 'Usage: API Gateway Server'],
-	['---------- TUNAS RIDEAN ----------', '', '', '', ''],
-	['tunas-epms-owned', '165.22.241.218', '22', 'root', 'Usage: Tunas E-PMS Owned'],
-	['tunas-staging-owned', '139.59.110.101', '22', 'root', 'Usage: Tunas E-PMS Staging'],
-	['---------- 7Spectrum ------------------------', '', '', '', ''],
-	['7spc-prod', '206.189.95.24', '35789', 'root', 'Usage: 7 Spectrum Production'],
+	['---------- Streamwave ------------------------', '', '', '', ''],
+	['streamwave-sgp1', '159.223.84.192', '22', 'root', 'Usage: Streamwave Web'],
 ]
 
 private_keys = ['/Users/ksetyadi/.ssh/id_rsa', '/Users/ksetyadi/.ssh/id_rsa_mbp_2011']
