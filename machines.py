c982db6f6ccbc56e05ab04fbbc29b7cbd9a98fae
Kristiono Setyadi <ksetyadi@tracehouse-go.local>
2025-04-09 06:19:52 +0700

    Update hosts

--- machines.py
+++ machines.py
@@ -3,14 +3,11 @@ hostnames = [
 	['orion-sgp1', '167.99.67.233', '22', 'root', 'Usage: Orion Server'],
 	['---------- Streamwave && Jurnalab ------------------------', '', '', '', ''],
 	['streamwave-sgp1', '159.223.84.192', '22', 'root', 'Usage: Streamwave Web'],
-	['---------- KolegaHR ------------------------', '', '', '', ''],
-	['kolegahr-staging', '152.42.227.21', '22', 'root', 'Usage: KolegaHR Staging'],
-	['kolegahr-facerec', '128.199.122.123', '22', 'root', 'Usage: KolegaHR Face Recognition'],
 	['---------- Bawaslu RI ------------------------', '', '', '', ''],
 	['bawaslu-dev', '159.65.1.3', '22', 'root', 'Usage: Bawaslu Absensi (Development - DigitalOcean)'],
 	['bawaslu-prod', '192.168.50.1', '22', 'absen', 'Usage: Bawaslu Absensi (Production - Bawaslu Server)'],
-	['---------- BUMN Today ------------------------', '', '', '', ''],
-	['bumntoday-dev', '152.42.188.204', '22', 'root', 'Usage: BUMN Today (Development)'], 
+	# ['---------- BUMN Today ------------------------', '', '', '', ''],
+	# ['bumntoday-dev', '152.42.188.204', '22', 'root', 'Usage: BUMN Today (Development)'], 
 ]
 
 private_keys = ['/Users/ksetyadi/.ssh/id_rsa', '/Users/ksetyadi/.ssh/id_rsa_mbp_2011']
