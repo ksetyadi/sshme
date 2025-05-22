826e844678f2ef83a974f8c72fcc06f0b91ef84a
Kristiono Setyadi <ksetyadi@tracehouse-go.local>
2025-05-22 22:22:29 +0700

    Update machines

--- machines.py
+++ machines.py
@@ -1,13 +1,9 @@
 hostnames = [
-	['---------- PERSONAL ----------', '-----', '--', '----', '---------'],
-	['orion-sgp1', '167.99.67.233', '22', 'root', 'Usage: Orion Server'],
-	['---------- Streamwave && Jurnalab ------------------------', '', '', '', ''],
-	['streamwave-sgp1', '159.223.84.192', '22', 'root', 'Usage: Streamwave Web'],
 	['---------- Bawaslu RI ------------------------', '', '', '', ''],
 	['bawaslu-dev', '159.65.1.3', '22', 'root', 'Usage: Bawaslu Absensi (Development - DigitalOcean)'],
 	['bawaslu-prod', '192.168.50.1', '22', 'absen', 'Usage: Bawaslu Absensi (Production - Bawaslu Server)'],
-	# ['---------- BUMN Today ------------------------', '', '', '', ''],
-	# ['bumntoday-dev', '152.42.188.204', '22', 'root', 'Usage: BUMN Today (Development)'], 
+	['---------- Efficiency Project ------------------------', '', '', '', ''],
+	['chila-gio-id', '103.127.139.165', '22', 'ksetyadi', 'Usage: Streamwave LP, Orion)'], 
 ]
 
 private_keys = ['/Users/ksetyadi/.ssh/id_rsa', '/Users/ksetyadi/.ssh/id_rsa_mbp_2011']
