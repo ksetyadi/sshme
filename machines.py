3bc10fc8d64b3a16f0ef4334e55c6be0556717ab
Kristiono Setyadi <ksetyadi@tracehouse-go.local>
2025-05-30 21:06:27 +0700

    Add gitea-gio-id

--- machines.py
+++ machines.py
@@ -4,6 +4,7 @@ hostnames = [
 	['bawaslu-prod', '192.168.50.1', '22', 'absen', 'Usage: Bawaslu Absensi (Production - Bawaslu Server)'],
 	['---------- Efficiency Project ------------------------', '', '', '', ''],
 	['chila-gio-id', '103.127.139.165', '22', 'ksetyadi', 'Usage: Streamwave LP, Orion)'], 
+	['gitea-gio-id', '202.74.74.99', '22', 'ksetyadi', 'Usage: Gitea Own Repository)'], 
 ]
 
 private_keys = ['/Users/ksetyadi/.ssh/id_rsa', '/Users/ksetyadi/.ssh/id_rsa_mbp_2011']
