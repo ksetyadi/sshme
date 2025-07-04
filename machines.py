34099af64b15ce70e0a462e2dd926bc958c8f0db
Kristiono Setyadi <ksetyadi@tracehouse-go.local>
2025-07-04 12:31:57 +0700

    Update machines.py

--- machines.py
+++ machines.py
@@ -3,8 +3,8 @@ hostnames = [
 	['bawaslu-dev', '159.65.1.3', '22', 'root', 'Usage: Bawaslu Absensi (Development - DigitalOcean)'],
 	['bawaslu-prod', '192.168.50.1', '22', 'absen', 'Usage: Bawaslu Absensi (Production - Bawaslu Server)'],
 	['---------- Efficiency Project ------------------------', '', '', '', ''],
-	['chila-gio-id', '103.127.139.165', '22', 'ksetyadi', 'Usage: Streamwave LP, Orion)'], 
-	['gitea-gio-id', '202.74.74.99', '22', 'ksetyadi', 'Usage: Gitea Own Repository)'], 
+	['chila-gio-id', '103.127.139.165', '22', 'ksetyadi', 'Usage: Streamwave LP, Orion'], 
+	['gitea-gio-id', '202.74.74.99', '22', 'ksetyadi', 'Usage: Gitea Own Repository'], 
 ]
 
 private_keys = ['/Users/ksetyadi/.ssh/id_rsa', '/Users/ksetyadi/.ssh/id_rsa_mbp_2011']
